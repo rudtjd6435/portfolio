9d3350f6c8550e17ace86430e9fa1d046ac27bcd
rudtjd6435 <rudtjd6435@naver.com>
2023-02-03 15:29:47 +0900

    first commit

---
+++ storr.js
@@ -0,0 +1,61 @@
+
+
+
+
+
+/***********************************************************/
+const Btn1 = document.querySelector('#btn1');
+const Btn2 = document.querySelector('#btn2');
+const Btn3 = document.querySelector('#btn3');
+const Btn4 = document.querySelector('#btn4');
+
+const Txt1 = document.querySelector('.txt1');
+const Txt2 = document.querySelector('.txt2');
+const Txt3 = document.querySelector('.txt3');
+const Txt4 = document.querySelector('.txt4');
+
+Btn1.addEventListener('click', () => Txt1.style.display = 'block');
+Btn1.addEventListener('click', () => Txt2.style.display = 'none');
+Btn1.addEventListener('click', () => Txt3.style.display = 'none');
+Btn1.addEventListener('click', () => Txt4.style.display = 'none');
+
+
+Btn2.addEventListener('click', () => Txt1.style.display = 'none');
+Btn2.addEventListener('click', () => Txt2.style.display = 'block');
+Btn2.addEventListener('click', () => Txt3.style.display = 'none');
+Btn2.addEventListener('click', () => Txt4.style.display = 'none');
+
+Btn3.addEventListener('click', () => Txt1.style.display = 'none');
+Btn3.addEventListener('click', () => Txt2.style.display = 'none');
+Btn3.addEventListener('click', () => Txt3.style.display = 'block');
+Btn3.addEventListener('click', () => Txt4.style.display = 'none');
+
+Btn4.addEventListener('click', () => Txt1.style.display = 'none');
+Btn4.addEventListener('click', () => Txt2.style.display = 'none');
+Btn4.addEventListener('click', () => Txt3.style.display = 'none');
+Btn4.addEventListener('click', () => Txt4.style.display = 'block');
+
+/*********************************************/
+var swiper = new Swiper(".mySwiper", { 
+  slidesPerView: 7,
+  spaceBetween: 3,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+delay: 600,
+stopOnLastSlide: false,
+disableOnInteraction: true,
+},
+});
+
+
+
