76b23ff125647f25579b11188836858b0441b992
rudtjd6435 <rudtjd6435@naver.com>
2023-02-07 14:59:25 +0900

    230207수정

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+$(function(){
+  // 스크롤 시 header fade-in
+  $(document).on('scroll', function(){
+      if($(window).scrollTop() > 100){
+          $("#header").removeClass("deactive");
+          $("#header").addClass("active");
+      }else{
+          $("#header").removeClass("active");
+          $("#header").addClass("deactive");
+      }
+  })
 
-
-
+});
 
 
 /***********************************************************/
